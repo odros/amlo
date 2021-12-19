1b346bc19ed54b5cfb1473faf6e0089e810e3fbe
S. S <44981224+odros@users.noreply.github.com>
2021-12-18 22:49:59 -0600

    Update aggregate.R

--- code/aggregate.R
+++ code/aggregate.R
@@ -1,19 +1,16 @@
+### This function aggregates a speech contained in a CSV file and returns a data frame
 
-# This function aggregates a speech contained in a CSV file and returns a data frame
-aggregate <- function(file) {
-  
+aggregate <- function(file)
+{
+  # Extract date from file name
   date <- substring(file, 13,22)
-  try({
-    
+  try
+  { 
     parsed_speech <- read.csv(file = file, header = TRUE) %>%
-      select(-X,turn) %>% # 
+      select(-X, turn) %>% # 
       group_by(speaker) %>% 
       summarize(speech = paste(speech, collapse = " \n ")) %>% # combine text of each speaker 
       mutate(Date = date, .before = speaker)
-    
-    
-  })
-  
+   }
   return(parsed_speech)
-  
 }
